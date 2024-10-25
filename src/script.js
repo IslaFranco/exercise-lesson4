9076ab5a4dcb1dc2d7dfe46bb5f3e38d5af7931e
IslaFranco <islafranco21@gmail.com>
2024-10-25 16:19:53 -0700

    Added gsap and animations module 5

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
+
 
 
 // Canvas
@@ -8,66 +10,66 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y = 1
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-cube2.position.x = -2
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
-)
-cube3.position.x = 2
-group.add(cube3)
-
+// Object  // basic box/cube 
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 
 // Axes Helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.position.y = 1
-// camera.position.x = 1
+
 scene.add(camera)
 
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+
+
+// Clock
+const clock = new THREE.Clock()
+
+
+
+//gsap
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
+
+
+// Animations
+
+const tick = () =>
+    {
+
+        //Clock
+        const elapsedTime = clock.getElapsedTime()
+
+        // Update Objects
+        camera.position.x = Math.cos(elapsedTime)
+        camera.position.y = Math.sin(elapsedTime)
+        camera.lookAt(mesh.position)
+
+        //Render
+        renderer.render(scene, camera)
+
+        window.requestAnimationFrame(tick)
+    }
+
+    tick()
